ae455616343d83945d43d60a9372330d93aff69f
Alejandro Mares Sánchez <31583644+A1xMares@users.noreply.github.com>
2024-07-05 03:15:00 -0600

    feat: improvements on SEO

--- src/js/aos.js
+++ src/js/aos.js
@@ -1,6 +1,6 @@
 import AOS from "aos";
 
 export function aosInit() {
-    AOS.init({ duration: 300, easing: "ease-in-out-sine", once: true, offset: 10 });
+    AOS.init({ duration: 300, easing: "ease-in-out-sine", once: true, offset: 250 });
 
 }
